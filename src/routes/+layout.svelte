f4b0119632118dbe7979c5bd573a02e49de39077
rishabh <gsyadav48@gmail.com>
2025-06-21 17:14:54 +0530

    getting to know the basics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<style>
+    .container{display: flex;}
+    .row{flex-direction: row;}
+    .column{flex-direction: column;}
+
+    .sidebar{
+        background-color: cornflowerblue;
+    }
+</style>
+
+<div class="parent container row">
+    <div class="sidebar column">
+asdfgheriofhiouwerh
+    </div>
+    <div>
+        <slot />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>test test test test</h1>
+<p>128yr89f23huewndj</p>
