71f59384bb6bb2aef83c436b10bee763f4d07a56
rishabh <gsyadav48@gmail.com>
2025-07-25 05:42:45 +0530

    laying out stuff for handles

--- src/routes/handles/+page.svelte
+++ src/routes/handles/+page.svelte
@@ -1,32 +1,51 @@
+<script>
+    let handlelist = [
+        {
+            name: "GitHub",
+            user: "@friedchaawal",
+            url: "https://github.com/friedchaawal",
+            icon: "fa-brands fa-github",
+            bg: "#0A0E12",
+            color: "#DDE7F0",
+        },
+        {
+            name: "DiscordServer",
+            user: "3 twinks and a top laner",
+            url: "https://discord.gg/swyVXNKWPF",
+            icon: "fa-brands fa-discord",
+            bg: "#7289da",
+            color: "#030303",
+        },
+        {
+            name: "Instagram",
+            user: "@fried_chaawal",
+            url: "https://www.instagram.com/fried_chaawal/",
+            icon: "fa-brands fa-instagram",
+            bg: "",
+            color: "#030303",
+        },
+        {
+            name: "YouTube",
+            user: "@fried_chaawal",
+            url: "https://www.youtube.com/channel/UCua-_UZDKJR74aDJncGJI3g",
+            icon: "fa-brands fa-youtube",
+            bg: "#D62A2A",
+            color: "#030303",
+        },
+        {
+            name: "Steam",
+            user: "Fried Chaawal",
+            url: "https://steamcommunity.com/id/fried_chaawal/",
+            icon: "fa-brands fa-steam",
+            bg: "",
+            color: "#FFFFFF",
+        },
+];      
+</script>
+
 <style>
-    h1  {
-        font-size: 4vmin;
-    }
-    p   {
-        font-size: 1.75vmin;
-        margin: 1vmin;
-    }
-    #Handles {
-        background-color: var(--color-base);
-        flex: 1;
-        flex-direction: row;
-    }
-    .handle-grid {
-    }
 </style>
-<div id="Handles">
-            <div>
-            <p>jdoiqwjodqw</p><p>iudfewhiudhweiu</p>
-        </div>
-                <div>
-            <p>jdoiqwjodqw</p><p>iudfewhiudhweiu</p>
-        </div>
-    <div class="handle-grid">
-        <div>
-            <p>jdoiqwjodqw</p><p>iudfewhiudhweiu</p>
-        </div>
-        <div>
-            <p>jdoiqwjodqw</p><p>iudfewhiudhweiu</p>
-        </div>
-    </div>
+
+<div class="handles-grid">
+    <p> 9iefwjouifjweio </p>
 </div>
